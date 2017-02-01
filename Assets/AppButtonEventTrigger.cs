0c0286aef803f0bf81eea731042cc65d4c96a901
BookSock <jasonseibel@Jasons-MacBook-Pro.local>
2017-01-31 20:18:21 -0500

    Commit two

---
+++ Assets/AppButtonEventTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+/*
+public class AppButtonEventTrigger : EventTrigger {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if RaycastHit == true {
+			
+
+			if (GvrController.AppButtonDown) {
+				Vector3 direction = Random.onUnitSphere;
+				direction.y = Mathf.Clamp(direction.y, 0.5f, 1f);
+				float distance = 2 * Random.value + 1.5f;
+				transform.localPosition = direction * distance;
+
+
+			// Do something.
+			// AppButtonUp is true for 1 frame after touchpad is touched.
+			}
+		}
+	
+	}
+}
+
+*/
